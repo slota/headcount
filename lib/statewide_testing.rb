79eaeabffb1e2702aeb10cfea2ea6220a0be22dc
John Slota <john.slota@gmail.com>
2015-09-30 21:26:34 -0600

    i am so tired

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -18,30 +18,33 @@ class StatewideTesting
       raise UnknownDataError
     end
     to_return = {}
-
     stats = CSV.open("../headcount/data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv", headers: true, header_converters: :symbol)
     stats.each do |row|
       district  = row[:location]
       score     = row[:score].downcase.to_sym
       year      = row[:timeframe].to_i
       stat_type = row[:dataformat]
-      value     = (row[:data].to_f * 1000).to_i / 1000.0
-
-      to_return[year] ||= {}
-      if stat_type == "Percent" && district == "ACADEMY 20"
-        to_return[year][score] = value
+      value     = row[:data]
+      if value != "#VALUE!" && value != "N/A" && value != nil && value != "LNE"
+        to_return[year] ||= {}
+        if stat_type == "Percent" && district == @data.fetch(1)[:location] && value != 0
+          to_return[year][score] = (value.to_f * 1000).to_i / 1000.0
+        end
       end
     end
-    to_return
+    to_return.reject { |key, value| value.empty? }
   end
 
   def proficient_by_race_or_ethnicity(race_input)
-    data_math    = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_Math.csv", headers: true, header_converters: :symbol
-    data_reading = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_Reading.csv", headers: true, header_converters: :symbol
-    data_writing = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_Writing.csv", headers: true, header_converters: :symbol
+    if race_input.to_s != "asian" && race_input.to_s != "black" && race_input.to_s != "pacific_islander" && race_input.to_s != "hispanic" && race_input.to_s != "native_american" && race_input.to_s != "two_or_more" && race_input.to_s != "white"
+      raise UnknownDataError
+    end
+    data_math    = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv", headers: true, header_converters: :symbol
+    data_reading = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv", headers: true, header_converters: :symbol
+    data_writing = CSV.open "../headcount/data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv", headers: true, header_converters: :symbol
     line = []
     return_lines = []
-    stats.each do |columns|
+    data_math.each do |columns|
       district  = columns[:location]
       race      = columns[:race_ethnicity]
       year      = columns[:timeframe]
